b0138aae4661b6b1cc230040589d270efb1b736e
Ija Saporenkova <ija.sha@gmail.com>
2024-08-15 12:28:03 +0100

    Performance improvements

--- Udemi_NetUnderTheHood/NewSolution/FastTableDataBuilder.cs
+++ Udemi_NetUnderTheHood/NewSolution/FastTableDataBuilder.cs
@@ -7,6 +7,44 @@ public class FastTableDataBuilder : ITableDataBuilder
 {
     public ITableData Build(CsvData csvData)
     {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        var resultRows = new List<Row>();
+
+        foreach (var row in csvData.Rows)
+        {
+            var newRow = new Row();
+
+            for (int columnIndex = 0; columnIndex < csvData.Columns.Length; ++columnIndex)
+            {
+                var column = csvData.Columns[columnIndex];
+                string valueAsString = row[columnIndex];
+
+                if (string.IsNullOrEmpty(valueAsString))
+                {
+                    continue;
+                }
+                else if (valueAsString == "TRUE")
+                {
+                    newRow.AssignCell(column, true);
+                }
+                else if (valueAsString == "FALSE")
+                {
+                    newRow.AssignCell(column, false);
+                }
+                else if (valueAsString.Contains(".") && decimal.TryParse(valueAsString, out var valueAsDecimal))
+                {
+                    newRow.AssignCell(column, valueAsDecimal);
+                }
+                else if (int.TryParse(valueAsString, out var valueAsInt))
+                {
+                    newRow.AssignCell(column, valueAsInt);
+                }
+                else newRow.AssignCell(column, valueAsString);
+            }
+
+            resultRows.Add(newRow);
+        }
+
+        return new TableData(csvData.Columns, resultRows);
     }
 }
---
+++ Udemi_NetUnderTheHood/NewSolution/Row.cs
@@ -0,0 +1,44 @@
+﻿//using Udemi_NetUnderTheHood.NewSolution;
+
+namespace CsvDataAccess.NewSolution;
+
+public class Row
+{
+    private Dictionary<string, string> _dataString = new();
+    private Dictionary<string, int> _dataInt = new();
+    private Dictionary<string, decimal> _dataDecimal = new();
+    private Dictionary<string, bool> _dataBool = new();
+
+    public void AssignCell (string columnName, bool value)
+    {
+        _dataBool[columnName] = value;
+    }
+
+    public void AssignCell(string columnName, int value)
+    {
+        _dataInt[columnName] = value;
+    }
+
+    public void AssignCell(string columnName, decimal value)
+    {
+        _dataDecimal[columnName] = value;
+    }
+
+    public void AssignCell(string columnName, string value)
+    {
+        _dataString[columnName] = value;
+    }
+
+    public object GetAtColumn(string columnName)
+    {
+        if (_dataBool.ContainsKey(columnName))
+            return _dataBool[columnName];
+        if (_dataInt.ContainsKey(columnName))
+            return _dataInt[columnName];
+        if (_dataDecimal.ContainsKey(columnName))
+            return _dataDecimal[columnName];
+        if (_dataString.ContainsKey(columnName))
+            return _dataString[columnName];
+        return null;
+    }
+}
