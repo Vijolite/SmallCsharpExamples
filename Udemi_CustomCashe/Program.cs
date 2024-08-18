2c96f2742547bc54446e95cd16ee5153b51268b1
Ija Saporenkova <ija.sha@gmail.com>
2024-08-18 11:02:51 +0100

    Changed in line with Udemi example

--- Udemi_CustomCashe/Program.cs
+++ Udemi_CustomCashe/Program.cs
@@ -1,54 +1,33 @@
 ﻿IDataDownloader dataDownloader = new SlowDataDownloader();
-var cashDictionary = new CacheDict<string, object>();
 
-Console.WriteLine(dataDownloader.DownloadData("id1", cashDictionary));
-Console.WriteLine(dataDownloader.DownloadData("id2", cashDictionary));
-Console.WriteLine(dataDownloader.DownloadData("id3", cashDictionary));
-Console.WriteLine(dataDownloader.DownloadData("id1", cashDictionary));
-Console.WriteLine(dataDownloader.DownloadData("id3", cashDictionary));
-Console.WriteLine(dataDownloader.DownloadData("id1", cashDictionary));
-Console.WriteLine(dataDownloader.DownloadData("id2", cashDictionary));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
 
 Console.ReadKey();
 
 public interface IDataDownloader
 {
-    string DownloadData(string resourceId, CacheDict<string, object> casheDict);
+    string DownloadData(string resourceId);
 }
 
 public class SlowDataDownloader : IDataDownloader
 {
-    public string DownloadData(string resourceId, CacheDict<string, object> casheDict)
+    private readonly CacheDict<string, string> _casheDict = new();
+    public string DownloadDataWithoutCashing(string resourceId)
     {
-        var downloadedUnit = new object();
-        if (!casheDict.IsInside(resourceId))
-        {
-            //let's imagine this method downloads real data,
-            //and it does it slowly
-            Thread.Sleep(1000);
-            downloadedUnit = new CasheDataUnit<object>(new object());
-            casheDict.AddToDict(resourceId, downloadedUnit);
-            return $"Some data for {resourceId} from download";
-        }
-        else
-        {
-            downloadedUnit = casheDict.GetById(resourceId);
-            return $"Some data for {resourceId} from cashe";
-        }
-
-        //return $"Some data for {resourceId}";
+        Thread.Sleep(1000);
+        return $"Some data for {resourceId}";
     }
-}
 
-public class CasheDataUnit <T>
-{
-    public T Value { get; set; }
-
-    public CasheDataUnit (T value)
+    public string DownloadData(string resourceId)
     {
-        Value = value;
+        return _casheDict.GetById(resourceId, DownloadDataWithoutCashing);
     }
-    
 }
 
 public class CacheDict <TKey, TValue>
@@ -59,18 +38,13 @@ public class CacheDict <TKey, TValue>
     {
         Data = new Dictionary<TKey, TValue>();
     }
-    public bool IsInside (TKey id)
-    {
-        return Data.ContainsKey(id);
-    }
 
-    public TValue GetById (TKey id)
+    public TValue GetById (TKey id, Func<TKey, TValue> getForTheFirstTime)
     {
+        if (!Data.ContainsKey(id))
+        {
+            Data[id] = getForTheFirstTime(id);
+        }
         return Data[id];
-    }
-
-    public void AddToDict (TKey id, TValue value)
-    {
-        Data[id] = value;
     }
 }
